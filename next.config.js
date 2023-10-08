30bcb0221930f2183a534441a5298c8c378f0a21
Samuel Oshio <samueloshio5@gmail.com>
2023-10-09 00:57:42 +0100

    config added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    apiUrl:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:8888' // development api
+        : 'http://localhost:8880', // production api
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
